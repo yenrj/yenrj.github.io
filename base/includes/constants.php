688b20c7f8f9c43d0ba1bcf9a8cab42e5f2aab0a
yenrj <kj83i1u2@gmail.com>
2018-08-23 13:09:44 +0800

    Revert "Revert "Revert "First website upload"""
    This reverts commit b7e6a11bca8729e3c622d36718eeba7f8a5a59f3.

--- base/includes/constants.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','kj83i1u2');
-define('DATABASE','');
-?>
--- base/includes/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once('constants.php');
-error_reporting(0);
-$con = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DATABASE);
-
-?>
